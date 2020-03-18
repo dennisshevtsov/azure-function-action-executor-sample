35b78169ef46edd9f1b432c6a2c40e4a9c34c91e
Dennis Shevtsov <dennisshevtsov@example.com>
2020-03-18 12:41:45 +0300

    create

---
+++ AzureFunctionActionExecutorSample.FunctionApp/HelloWorldActionExecutor.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+namespace AzureFunctionActionExecutorSample.FunctionApp
+{
+  using System.Net.Mime;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  using Microsoft.AspNetCore.Http;
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+  internal sealed class HelloWorldActionExecutor : IActionResultExecutor<ObjectResult>
+  {
+    public Task ExecuteAsync(ActionContext context, ObjectResult result)
+      => WriteAsync(context.HttpContext.Response);
+
+    private static async Task WriteAsync(HttpResponse response)
+    {
+      response.StatusCode = StatusCodes.Status200OK;
+      response.ContentType = MediaTypeNames.Application.Json;
+
+      var bytes = Encoding.UTF8.GetBytes("{ \"message\": \"Hello World\" }");
+
+      await response.Body.WriteAsync(bytes);
+    }
+  }
+}
---
+++ AzureFunctionActionExecutorSample.FunctionApp/Startup.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+[assembly: Microsoft.Azure.WebJobs.Hosting.WebJobsStartup(typeof(AzureFunctionActionExecutorSample.FunctionApp.Startup))]
+
+namespace AzureFunctionActionExecutorSample.FunctionApp
+{
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.AspNetCore.Mvc.Infrastructure;
+  using Microsoft.Azure.WebJobs;
+  using Microsoft.Azure.WebJobs.Hosting;
+  using Microsoft.Extensions.DependencyInjection;
+
+  public sealed class Startup : IWebJobsStartup
+  {
+    public void Configure(IWebJobsBuilder builder)
+      => builder.Services.AddSingleton<IActionResultExecutor<ObjectResult>, HelloWorldActionExecutor>();
+  }
+}
